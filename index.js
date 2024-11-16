4a8d5a052e48c205250beb1101c66197f2a99341
= <=>
2024-11-16 17:57:18 +0200

    Kayan-0.2

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+
+
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {console.log(`Server is running at port ${PORT}`)});
